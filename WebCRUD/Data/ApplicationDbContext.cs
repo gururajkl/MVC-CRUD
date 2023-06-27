d10a21b101356b7305574c2affc6e1be1e773660
Gururaj KL <gururaj.kl@sasken.com>
2023-06-27 18:43:44 +0530

    Updated with DbContext

---
+++ WebCRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebCRUD.Models;
+
+namespace WebCRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
