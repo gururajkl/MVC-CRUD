def3f022a9a7a41ba13dbf00b849821343155ed5
Gururaj KL <gururaj.kl@sasken.com>
2023-06-28 08:52:27 +0530

    Updated with Migrations and Views

--- WebCRUD/Program.cs
+++ WebCRUD/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebCRUD.Data;
+
 namespace WebCRUD
 {
     public class Program
@@ -8,6 +11,10 @@ namespace WebCRUD
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer
+            (
+                builder.Configuration.GetConnectionString("lenovo")
+            ));
 
             var app = builder.Build();
 
