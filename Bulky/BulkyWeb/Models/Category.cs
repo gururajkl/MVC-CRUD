c5d5c0a49b6e546c37ca24153ef12a905e82d32f
Gururaj KL <gururaj.kl@sasken.com>
2023-06-29 12:08:39 +0530

    Updated with N-tier architecture

---
+++ Bulky/BulkyWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        public string? Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display order should between 1 and 100 only!!")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
